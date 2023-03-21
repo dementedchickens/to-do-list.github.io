9b1c4e0fc25c26b314b5b4f02559a9bddb75d34e
dementedchickens <123401219+dementedchickens@users.noreply.github.com>
2023-03-21 11:43:31 -0500

    list babes
    we go it to list

--- list.js
+++ list.js
@@ -1,1 +1,7 @@
-list.js
+function moveToMyList() {
+  var text = document.getElementById("textbox").value; // Get the text from the textbox
+  var node = document.createElement("LI"); // Create a new list item
+  var textnode = document.createTextNode(text); // Create a text node with the text from the textbox
+  node.appendChild(textnode); // Add the text node to the list item
+  document.getElementById("myList").appendChild(node); // Add the list item to the list
+}
