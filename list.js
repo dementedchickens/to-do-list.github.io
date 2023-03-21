1eca55cffa864707f1989959f56a72ac28769c3a
dementedchickens <123401219+dementedchickens@users.noreply.github.com>
2023-03-21 11:55:27 -0500

    dbaerteabarb
    gbrtrstns

--- list.js
+++ list.js
@@ -1,7 +1,7 @@
 function moveToMyList() {
-  var text = document.getElementById("textbox").value; // Get the text from the textbox
-  var node = document.createElement("LI"); // Create a new list item
-  var textnode = document.createTextNode(text); // Create a text node with the text from the textbox
-  node.appendChild(textnode); // Add the text node to the list item
-  document.getElementById("myList").appendChild(node); // Add the list item to the list
+  var text = document.getElementById("textbox").value; 
+  var node = document.createElement("LI"); 
+  var textnode = document.createTextNode(text); 
+  node.appendChild(textnode); 
+  document.getElementById("myList").appendChild(node); 
 }
