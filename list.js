d0a4a8303ae9fa3c72ee860545ffb33ca974e02c
dementedchickens <123401219+dementedchickens@users.noreply.github.com>
2023-04-17 11:09:03 -0500

    explainations

--- list.js
+++ list.js
@@ -1,15 +1,15 @@
 function addToList() {
  
   const input = document.getElementById("textInput");
-  const inputValue = input.value;
+  const inputValue = input.value;//this gets the "value" or the words that was put into the textbox
 
   const listItem = document.createElement("li");
   const textNode = document.createTextNode(inputValue);
-  listItem.appendChild(textNode);
+  listItem.appendChild(textNode);//creates a new lsit item with the value of the textbox
 
   
   const list = document.getElementById("list");
-  list.appendChild(listItem);
+  list.appendChild(listItem);//adds the new item to the list.
 
 
   listItem.addEventListener("mouseover", () => {
@@ -18,7 +18,7 @@ function addToList() {
   listItem.addEventListener("mouseout", () => {
     listItem.classList.remove("active");
   });
-  listItem.addEventListener("click", () => {
+  listItem.addEventListener("click", () => {//sdds the lsiterner events and what's going to trigger the changes in the list in this case its the pop up
     const action = prompt("Do you want to move the item up, move it down, or delete it? (up/down/delete)");
     if (action === "up") {
       const prevItem = listItem.previousElementSibling;
@@ -33,7 +33,7 @@ function addToList() {
     } else if (action === "delete") {
       list.removeChild(listItem);
     }
-  });
+  });// this entire section basically takes the imput of the textbox and depending if it is up, down, or delete the action asigned to the words will happen to the task
 
  
   input.value = "";
@@ -45,5 +45,5 @@ function clearList() {
   while (list.firstChild) {
     list.removeChild(list.firstChild);
   }
-}
+}//the function that remove all of the stuff from the list.
 
